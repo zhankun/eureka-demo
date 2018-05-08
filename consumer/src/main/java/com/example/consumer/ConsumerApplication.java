662bff2551ddf24bd763e3bd06c5cdd4d6cfff06
zhankun <kun_zhan1989@163.com>
2018-05-08 11:48:39 +0800

    fix

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -7,6 +7,11 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author zhankun
+ * @Description 使用restTemplate来调用服务
+ * @Date 2018/5/8 10:06
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConsumerApplication {
